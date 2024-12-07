1d907e81ed97aff93591728c0b3cf93ef456ed06
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-12-07 23:14:42 +0100

    Add solution for day 7 task 2

--- src/day7.c
+++ src/day7.c
@@ -32,7 +32,7 @@ int read_input_line(FILE* input_f, vec_int* values, long* target) {
   return 0;
 }
 
-int production_possibilities(vec_int* values, long result, long first_n) {
+int production_possibilities(vec_int* values, long result, long first_n, bool allow_concat) {
   if (first_n == 1) {
     if (values->size == 0) {
       return 0;
@@ -42,24 +42,44 @@ int production_possibilities(vec_int* values, long result, long first_n) {
   }
   int v = values->start[first_n - 1];
   
-  int res_mul, res_add;
+  int res_mul = 0, res_add = 0, res_concat = 0;
 
-  if (result % v != 0) {
     // if the result is not divisible by
     // the last value, then it was surely not
     // produced by multiplication with this value
     res_mul = 0;
-  } else {
-    res_mul = production_possibilities(values, result / v, first_n - 1);
+  if (result % v == 0) {
+    res_mul = production_possibilities(
+       values, 
+       result / v, 
+       first_n - 1, 
+       allow_concat
+     );
   }
 
-  if (result - v < 0) {
-    res_add = 0;
-  } else {
-    res_add = production_possibilities(values, result - v, first_n - 1);
+  if (result - v >= 0) {
+    res_add = production_possibilities(
+       values, 
+       result - v, 
+       first_n - 1, 
+       allow_concat
+     );
+  }
+
+  if (allow_concat) {
+    int mask = 1;
+    do{
+      mask *= 10;
+    } while (v / mask > 0);
+    if (result % mask == v)
+      res_concat = production_possibilities(
+        values, 
+        result / mask, 
+        first_n - 1, 
+        allow_concat);
   }
   
-  return res_mul + res_add;
+  return res_mul + res_add + res_concat;
 } 
 
 
@@ -68,18 +88,23 @@ int day7() {
   assert(input_f);
   
   vec_int values = MK_VEC(int);
-  long result = 0, target_sum, minimizer;
+  long result = 0, result_t2 = 0, target_sum;
+
   while(!read_input_line(input_f, &values, &target_sum)) {
-    int poss = production_possibilities(&values, target_sum, values.size);
+    int poss = production_possibilities(&values, target_sum, values.size, false);
+    int poss_t2 = production_possibilities(&values, target_sum, values.size,true);
     
     result  +=  poss != 0 ? target_sum : 0;
+    result_t2 += poss_t2 != 0 ? target_sum : 0;
+
     // printf("Target sum: %ld", target_sum);
     
     // clear the vec
     values.size = 0;
   }
 
-  printf("---> Q1: The sum of numbers that can be produced is: %ld", result);
+  printf("---> Q1: The sum of numbers that can be produced is: %ld\n", result);
+  printf("---> Q2: The sum of numbers that can be produced is: %ld\n", result_t2);
   
 
   free(values.start);
